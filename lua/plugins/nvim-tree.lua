7c3d9eea7cb79a10907e937c1bc710dc76b1e10c
Sayem <sayem.afm@dtone.com>
2022-11-13 16:13:24 +0800

    fix: update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -12,39 +12,52 @@
 --- setup function:
 --- https://github.com/kyazdani42/nvim-tree.lua#setup
 --- See: `help NvimTree`
-local g = vim.g
+-- local g = vim.g
 
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_width_allow_resize  = 1
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1
-}
+-- g.nvim_tree_git_hl = 1
+-- g.nvim_tree_highlight_opened_files = 1
+-- g.nvim_tree_respect_buf_cwd = 1
+-- g.nvim_tree_width_allow_resize  = 1
+-- g.nvim_tree_show_icons = {
+--   git = 1,
+--   folders = 1,
+--   files = 1
+-- }
 
-g.nvim_tree_icons = {
-	default = "‣ "
-}
+-- g.nvim_tree_icons = {
+-- }
 
 require('nvim-tree').setup {
   open_on_setup = true,
   update_cwd = true,
+  respect_buf_cwd = true,
   renderer = {
     indent_markers = {
       enable = true
+    },
+    highlight_git = true,
+    icons = {
+      glyphs = {
+        default = "‣ "
+      },
+      show = {
+        git = true,
+        folder = true,
+        file = true
+      }
     }
   },
-  open_file = {
-    quit_on_open = false,
-    window_picker = {
-      enable = false
+  actions = {
+    open_file = {
+      quit_on_open = false,
+      resize_window = true,
+      window_picker = {
+        enable = false
+      }
     }
   },
   view = {
-    width = 32,
-    auto_resize = true
+    width = 32
   },
   filters = {
     dotfiles = true,
