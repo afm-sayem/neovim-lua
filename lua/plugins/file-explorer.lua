d97ca08c77e921d3bb4ac479cf80055f0ad96980
lemonchiffon <lemonchiffon@shotero.com>
2023-07-27 11:06:20 +0800

    fileexplorer: show hidden files

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -1,9 +1,17 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_hidden = false,
+			},
+		},
+	},
 }
